43025fa86e957fb0c21681858f8c33d501556adf
Dmitriy Karzakov <dk@didlogic.com>
2025-02-04 19:07:54 +0300

    changed routes

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -8,18 +8,18 @@ import { CreateLinkDto } from "./dto/create-link.dto"
 export class LinkController {
   constructor(private linkService: LinkService) {}
 
-  @Post("/")
+  @Post()
   shortUrl(@Body() data: CreateLinkDto) {
     return this.linkService.createShortUrl(data)
   }
 
-  @Get("/:code")
+  @Get(":code")
   @Redirect("https://lshort.ru", 302)
   redirect(@Param() params: { code: string }) {
     return this.linkService.handleRedirect(params.code)
   }
 
-  @Get("/clicks/:code")
+  @Get("clicks/:code")
   clicksCount(@Param() params: { code: string }) {
     return this.linkService.getLinkClicks(params.code)
   }
