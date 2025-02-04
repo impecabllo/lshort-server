92d84f917b5f9260f39546028e094e06f5a36932
Dmitriy Karzakov <dk@didlogic.com>
2025-02-04 18:55:30 +0300

    edited cors

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ import { ValidationPipe } from "@nestjs/common"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-
-  const whitelist = ["https://lshort.ru", "https://www.lshort.ru"]
-
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === "development" || !origin) {
-        callback(null, true)
-      } else {
-        console.error(origin)
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   })
+
+  // const whitelist = ["https://lshort.ru", "https://www.lshort.ru"]
+
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === "development" || !origin) {
+  //       callback(null, true)
+  //     } else {
+  //       console.error(origin)
+  //       callback(new Error("Not allowed by CORS"))
+  //     }
+  //   },
+  //   credentials: true,
+  // })
 
   app.useGlobalPipes(
     new ValidationPipe({
