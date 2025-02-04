6ee89118c6498fe012e0266bbe55a748fce12e59
Dmitriy Karzakov <dk@didlogic.com>
2025-02-04 19:03:40 +0300

    changed cors

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,19 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  const whitelist = ["https://lshort.ru", "https://www.lshort.ru"]
 
-  // const whitelist = ["https://lshort.ru", "https://www.lshort.ru"]
-
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === "development" || !origin) {
-  //       callback(null, true)
-  //     } else {
-  //       console.error(origin)
-  //       callback(new Error("Not allowed by CORS"))
-  //     }
-  //   },
-  //   credentials: true,
-  // })
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === "development" || !origin) {
+        callback(null, true)
+      } else {
+        console.error(origin)
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
