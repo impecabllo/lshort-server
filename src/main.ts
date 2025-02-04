1ec7b5ea2d81cf31c29bcd11ce5f9a58f526a4d6
Dmitriy Karzakov <dk@didlogic.com>
2025-02-04 18:58:54 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { ValidationPipe } from "@nestjs/common"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  })
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
 
   // const whitelist = ["https://lshort.ru", "https://www.lshort.ru"]
 
